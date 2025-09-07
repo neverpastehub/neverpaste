36c35642fdd524547b4d26fcec8440e91c7b1a1b
neverpastehub <neverpastehub@gmail.com>
2025-09-07 09:07:21 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+local scripts = {
+	[137925884276740] = "https://raw.githubusercontent.com/neverpastehub/neverpaste/refs/heads/main/buildaplane.lua",
+}
+
+loadstring(game:HttpGet(scripts[game.PlaceId]))()
